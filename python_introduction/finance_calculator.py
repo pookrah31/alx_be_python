ce24e71d95426ad8083521f1ba957e7cb9b716b1
Pookrah31 <penielokrah31@gmail.com>
2025-11-09 01:16:42 +0000

    Fix: corrected print format

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,5 @@ savings_monthly = income_monthly - expenses_monthly
 
 projected_savings = savings_monthly  * 12 + (savings_monthly *12 * 0.05)
 
-print(f"Your monthly savings are ${int(savings_monthly)}")
-print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}")
+print(f"Your monthly savings are ${int(savings_monthly)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
