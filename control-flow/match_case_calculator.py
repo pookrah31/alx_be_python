3155086c4c2878ad52887c28961210cffa8ce184
Pookrah31 <penielokrah31@gmail.com>
2025-11-14 13:47:16 +0000

    Match case calculator script

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /): ").strip().lower()
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print (f"The result is {result}.")
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result is {result}.")   
+        
