5f558475e18d031669c5b5b78193cde7f32aa74a
Pookrah31 <penielokrah31@gmail.com>
2025-11-15 15:35:05 +0000

    Fix variable naming

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
-Task = input("Enter your task: " )
-Priority = input("Priority (high/medium/low): ").strip().lower()
-TimeBound = input("Is it time bound? (yes/no): ").strip().lower()
+task = input("Enter your task: " )
+priority = input("Priority (high/medium/low): ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
 
 # Using match-case to provide remainder based on priority
-match Priority:
+match priority:
     case "high":
-        message = f"'{Task}' is a high priority task"
+        message = f"'{task}' is a high priority task"
     case "medium":
-        message = f"'{Task}' is a medium priority task"
+        message = f"'{task}' is a medium priority task"
     case "low":
-        message = f"'{Task}' is a low priority task"
+        message = f"'{task}' is a low priority task"
     case _:
-        message = f"'{Task}' has an undefined priority"
+        message = f"'{task}' has an undefined priority"
 
 
 
-if TimeBound == "yes":
-    message =f"Reminder: {message} that requires immmediate attention today!"
+if time_bound == "yes":
+    message =f"Reminder: {message} that requires immediate attention today!"
 else:
     message = f"Note: {message}. Consider completing it when you have free time."
 print(message)
