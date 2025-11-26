8922f610cdb1dabab3e65e4e2f2d05788a2e3482
Pookrah31 <penielokrah31@gmail.com>
2025-11-26 12:47:33 +0000

    Fix: Implentation of display_balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,5 +23,5 @@ class BankAccount:
             return True
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
