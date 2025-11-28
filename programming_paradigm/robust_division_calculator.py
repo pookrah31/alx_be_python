20a7d72fa9779345e2b493fc26e7f5decae6ef93
Pookrah31 <penielokrah31@gmail.com>
2025-11-28 08:13:24 +0000

    Fix: Error in Implementation of ValueError

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator, denominator):
 
         try:
            result = numerator / denominator
-           return f" The result of the division is {result}"
+           return f"The result of the division is {result}"
 
         except ZeroDivisionError:
           return "Error: Cannot divide by zero."
