3cf130145c4403117ba6ef6bddc94b0ed47a93ad
Pookrah31 <penielokrah31@gmail.com>
2025-11-28 08:11:50 +0000

    Fix: Error in Implementation of ValueError

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -12,5 +12,5 @@ def safe_divide(numerator, denominator):
           return "Error: Cannot divide by zero."
     
     except ValueError:
-       return "Error:  Please enter numeric values only."
+       return "Error: Please enter numeric values only."
 
