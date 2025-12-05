dd9125d9ba9fe20fe6997befca51bdc35088e79f
Pookrah31 <penielokrah31@gmail.com>
2025-12-05 20:44:33 +0000

    Add Shape base class and Rectangle/Circle subclasses with polymorphic area() methods

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -10,7 +10,7 @@ class Rectangle(Shape):
         self.length = length
 
     def area(self):
-        return self.width * self.length
+        return self.length * self.width
     
 class Circle(Shape):
     def __init__ (self, radius):  
