7d2fbf0f94cf3c3d7916db357f79723944317452
Pookrah31 <penielokrah31@gmail.com>
2025-12-05 16:48:44 +0000

    Fix: removes extra space in output

--- oop/library_system.py
+++ oop/library_system.py
@@ -28,7 +28,7 @@ class Library:
     def list_books(self):
         for book in self.books:
             if isinstance(book, EBook):
-                print(f"Ebook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
